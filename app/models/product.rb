443e51dd2b3132cb55dd8194a3a3baf355fc0b16
Adam Okoń <adam.okon@elegantcode.pl>
2024-03-28 20:58:40 +0100

    play with broadcasts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
   enum :color, [:red, :blue, :black]
   belongs_to :category
+
+  after_commit :broadcast_changes
+
+  private
+
+  def broadcast_changes
+    broadcast_replace_to "inbox", partial: "products/product",
+      locals: {product: self, category: category}, target: "product_#{id}"
+  end
 end
